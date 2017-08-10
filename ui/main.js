c3d73519e0d57c452aebdc5fe6bab3af18ae0f4a
jesthi-sanjeeta <30695277+jesthi-sanjeeta@users.noreply.github.com>
2017-08-11 00:09:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,6 +48,6 @@ submit.onClick = function() {
 };
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-request.open('GET','http://jesthisanjeeta.imad.hasura-app.io/submit-name='+ name,true);
+request.open('GET','http://jesthisanjeeta.imad.hasura-app.io/submit-name?name='+ name,true);
  request.send(null);
 };
