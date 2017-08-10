735305fb8ff590ee72e57cf493635eecb600abee
jesthi-sanjeeta <30695277+jesthi-sanjeeta@users.noreply.github.com>
2017-08-10 18:43:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@
 var button = document.getElementById('counter');
 //var counter = 0;
 button.onclick = function() {
-  //MAke a request to th ecounter endpoint 
+  //Create a request Object 
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
    if(request.readystate == XMLHttpRequest.DONE) {
@@ -31,4 +31,7 @@ button.onclick = function() {
   //counter = counter + 1;
   //var span = document.getElementById('count');
  // span.innerHTML = counter.toString();
+ //Make a request 
+ request.open('GET','http://sanjeeta-jesthi.imad.hasura-app.io/counter',true);
+ request.send(null);
 };
