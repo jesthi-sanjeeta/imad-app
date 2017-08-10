89d565ebc9e504b7c0a6df8f908313bb35ecae62
jesthi-sanjeeta <30695277+jesthi-sanjeeta@users.noreply.github.com>
2017-08-10 18:49:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,11 @@
-//console.log('Loaded!');
-//var element = document.getElementById('main-text');
-//element.innerHTML='New Value';
-//var img = document.getElementById("madi");
-//var marginLeft =0;
-//function moveRight(){
-  //  marginLeft=marginLeft+1;
-    //img.style.marginLeft = marginLeft + 'px';
-//}
-//img.onclick =function(){
-    //img.style.marginLeft='100px';
-   // var interval = setInterval(moveRight,50);
-//};
+
 var button = document.getElementById('counter');
-//var counter = 0;
+
 button.onclick = function() {
   //Create a request Object 
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
-   if(request.readystate == XMLHttpRequest.DONE) {
+   if(request.readyState == XMLHttpRequest.DONE) {
        if(request.ststus == 200){
            var counter = request.responseText;
            var span = document.getElementById('count');
