7784deba8c5bfa3c9bfb21498833b7b5372f8b4f
jesthi-sanjeeta <30695277+jesthi-sanjeeta@users.noreply.github.com>
2017-08-10 23:57:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,16 +26,26 @@ button.onclick = function() {
 //Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-var submit = document.getElementById('submit');
+var submit = document.getElementById('submit_btn');
 submit.onClick = function() {
     //Make a request to server nd send name
+    var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+   if(request.readyState === XMLHttpRequest.DONE) {
+       if(request.status === 200){
     //Capture a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list ='';
     for(var i = 0 ;i<names.length;i++)
     {
         list +='<li>' +name[i]+'</li>';
     }
+       }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+   }
+  }
 };
+request.open('GET','http://jesthisanjeeta.imad.hasura-app.io/submit-name='+ name,true);
+ request.send(null);
