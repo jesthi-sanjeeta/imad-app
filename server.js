c91d689b18dcac6f04972e5b897dc398cf215c57
jesthi-sanjeeta <30695277+jesthi-sanjeeta@users.noreply.github.com>
2017-08-21 19:57:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,6 +133,29 @@ app.post('/create-user',function(req,res){
         }
     });
 });
+app.post('/login',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" username =$1', [username], function(err,result){
+        if(err){
+            res.status(500).send(err,toString());
+        }
+        else{
+            if(result.rows.lenght === 0){
+                res.status(403).send('Username/Password is invalid');
+            }else{
+                var dbString =result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password,salt);
+                if(hashedPassword === dbString){
+                 res.send('Credentials correct !');
+                }else{
+                    res.status(403).send('Username/Password is invalid');
+                }
+            }
+        }
+    });
+});
 var pool = new Pool(config);
 app.get('/test_db',function(req,res){
 //make a select request
